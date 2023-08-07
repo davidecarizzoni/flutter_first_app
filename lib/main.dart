d91ba4eed4356584ba52343eb05487420de39a11
davidecarizzoni <davide.carizzoni@runelab.it>
2023-08-07 15:38:21 +0200

    chore(mainApp): run app first widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,13 @@
 import 'package:flutter/material.dart';
 
+//parse and compiled from top to bottom
+//main function are executed automatically
+
+// widget are nested and compiled to create the user interface of our application
 void main() {
-  runApp(const MyApp());
-}
-
-// Run from top to bottom and then compiled into specifi platform language (native ios and android for example)
-// keyword and identifiers
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My first flutter aoo'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  // runApp is called inside main function
+  // materialApp is main widget, used as starting point
+  runApp(const MaterialApp(
+    home: Text('Hello world!'),
+  ));
 }
